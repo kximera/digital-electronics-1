58d4b1d8ba07513ea75a939762f485ab6cfceb67
kximera <63504192+kximera@users.noreply.github.com>
2023-03-21 21:38:07 +0100

    Add files via upload

---
+++ 06-counter/project_1/project_1.srcs/sources_1/new/cnt_up_down.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/15/2023 11:33:57 AM
+-- Design Name: 
+-- Module Name: cnt_up_down - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity cnt_up_down is
+--  Port ( );
+end cnt_up_down;
+
+architecture Behavioral of cnt_up_down is
+
+begin
+
+
+end Behavioral;
